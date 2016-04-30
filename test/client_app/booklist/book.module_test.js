b2cd4a9272063d288fec078df30315545206fe2b
Hares Faiez <faiez.hrs@gmail.com>
2016-04-30 16:52:31 +0100

    Test fetching books for category service

--- test/client_app/booklist/book.module_test.js
+++ test/client_app/booklist/book.module_test.js
@@ -1,21 +1,35 @@
 describe('Books', function(){
   var $controller;
+  var $httpBackend;
+  var fetchBooksForCategory;
+  var fakeFetch = {books: ['Angular in action']};
 
   beforeEach(module('Books'));
+
+  beforeEach(inject(function(FetchBooksForCategory, _$httpBackend_) {
+    fetchbooksforcategory = FetchBooksForCategory;
+    $httpBackend = _$httpBackend_;
+  }));
 
   beforeEach(inject(function(_$controller_){
     $controller = _$controller_;
   }));
 
   describe('per category', function(){
+                it('should fetch category of books from the service', function(){
+                    $httpBackend.when('GET', '/category/all').respond(fakeFetch);
+                    $httpBackend.expectGET('/category/all');
+                    fetchbooksforcategory.fetch();
+                    $httpBackend.flush();
+                });
+
                 it('should be in an horizontal list', function(){
-                     var fakeFetch = {books: ['Angular in action']};
                      var $scope = {};
                      var listBooks = $controller('BookPerCategory',
                                                  { $scope:   $scope,
                                                    FetchBooksForCategory: fakeFetch });
 
-                     expect($scope.elements).toEqual(fakeFetch.books);
+                    expect($scope.elements).toEqual(fakeFetch.books);
                 });
            });
 });
