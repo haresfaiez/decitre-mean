8e0b4a358c9b157febe73b418e1137b53a7838c5
Hares Faiez <faiez.hrs@gmail.com>
2016-04-30 17:17:37 +0100

    Return the function instead of an object in the fetch service

--- client_app/booklist/booklist.module.js
+++ client_app/booklist/booklist.module.js
@@ -2,21 +2,20 @@ angular.module('Books', []);
 
 angular.module('Books')
        .factory('FetchBooksForCategory', function($http){
-  var result = {
-                fetch: function(handle){
-                  var target = '/category/all';
-                  var promise = $http.get(target).
-                    success(function(data, status, headers, config) {
-                                  handle(data);
-                                })}
-               };
+  var result = function(handle){
+                  var target     = '/category/all';
+                  var handleData = function(data, status, headers, config) {
+                                       handle(data);
+                                  };
+                  $http.get(target).success(handleData);
+                };
   return result;
 });
 
 angular.module('Books')
        .controller('BookPerCategory', function($scope,
                                                FetchBooksForCategory){
-  FetchBooksForCategory.fetch(function(data){
-    $scope.elements = data;
+  FetchBooksForCategory(function(books){
+    $scope.elements = books;
   });
 });
