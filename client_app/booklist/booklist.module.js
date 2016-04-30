2373533fc0aa82461940b47b8e80e4159ceb04e8
Hares Faiez <faiez.hrs@gmail.com>
2016-04-30 17:30:51 +0100

    Format code

--- client_app/booklist/booklist.module.js
+++ client_app/booklist/booklist.module.js
@@ -1,21 +1,21 @@
 angular.module('Books', []);
 
 angular.module('Books')
-       .factory('categoryBooks', function($http){
+       .factory('CategoryBooks', function($http){
   var result = function(categoryId, handle){
-                  var target     = '/category/all';
-                  var handleData = function(data, status, headers, config) {
-                                       handle(data);
-                                  };
-                  $http.get(target).success(handleData);
+                 var target       = '/category/all';
+                 var handleResult = function(data, status, headers, config) {
+                                      handle(data);
+                                    };
+                 $http.get(target).success(handleResult);
                 };
   return result;
 });
 
 angular.module('Books')
-       .controller('BookPerCategory', function($scope,
-                                               categoryBooks){
-  categoryBooks(1, function(books){
-    $scope.elements = books;
-  });
+       .controller('BindCategoryBooks', function($scope, CategoryBooks){
+  var bind = function(books){
+               $scope.elements = books;
+             };
+  CategoryBooks(1, bind);
 });
--- test/client_app/booklist/book.module_test.js
+++ test/client_app/booklist/book.module_test.js
@@ -1,25 +1,25 @@
 describe('Books', function(){
   var $controller;
   var $httpBackend;
-  var fakeFetch = {books: ['Angular in action', 'Getting MEAN']};
+  var category = {books: ['Angular in action', 'Getting MEAN']};
 
   beforeEach(module('Books'));
 
   beforeEach(inject(function(_$controller_, _$httpBackend_){
-    $controller = _$controller_;
+    $controller  = _$controller_;
     $httpBackend = _$httpBackend_;
   }));
 
   describe('for one category', function(){
                 it('should fetch books of a category', function(){
-                    $httpBackend.when('GET', '/category/all').respond(fakeFetch);
+                    $httpBackend.when('GET', '/category/all').respond(category);
                     $httpBackend.expectGET('/category/all');
-                    var $scope = {};
-                    var listBooks = $controller('BookPerCategory',
+                    var $scope    = {};
+                    var listBooks = $controller('BindCategoryBooks',
                                                 {$scope: $scope});
                     $httpBackend.flush();
 
-                    expect($scope.elements).toEqual(fakeFetch);
+                    expect($scope.elements).toEqual(category);
                 });
            });
 });
