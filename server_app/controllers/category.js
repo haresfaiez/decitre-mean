0d3aabc994dc7ed50f978480a5df8cc1c2987e32
Hares Faiez <faiez.hrs@gmail.com>
2016-05-01 15:41:28 +0100

    Retrieve categories from the database

--- server_app/controllers/category.js
+++ server_app/controllers/category.js
@@ -1,9 +1,17 @@
+var mongoose       = require('mongoose');
+
+var bookSchema     = mongoose.model('Book');
+var categorySchema = mongoose.model('Category');
+
 var booksPerCategory = function(req, res){
   var books = [{category: "javascript", books: ['Angular in action', 'Getting MEAN']},
                {category: "scala", books: ['Scala in action', 'Getting Scala']}];
+  categorySchema.find({}, function(err, result) {
+                        console.log('categories' + result);
+                        res.status(200);
+                        res.json(books);
+                      });
 
-  res.status(200);
-  res.json(books);
 }
 
 module.exports.all = booksPerCategory;
