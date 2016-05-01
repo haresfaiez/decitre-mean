bc199b5c4dad9221e371c317ad3a6a436c60a216
Hares Faiez <faiez.hrs@gmail.com>
2016-05-01 14:59:13 +0100

    Rename categories fetching handler in the category controller

--- server_app/controllers/category.js
+++ server_app/controllers/category.js
@@ -1,9 +1,9 @@
 var booksPerCategory = function(req, res){
   var books = [{category: "javascript", books: ['Angular in action', 'Getting MEAN']},
                {category: "scala", books: ['Scala in action', 'Getting Scala']}];
- 
+
   res.status(200);
   res.json(books);
 }
 
-module.exports.books = booksPerCategory;
+module.exports.all = booksPerCategory;
--- server_app/routes/index.js
+++ server_app/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
 var main     = require('../controllers/main');
 var category = require('../controllers/category');
 
 router.get('/', main.angular);
-router.get('/category/books', category.books);
+
+router.get('/category/books', category.all);
 
 module.exports = router;
