dbfb073b7bdf98249555be671cb1d54cc478158c
Hares Faiez <faiez.hrs@gmail.com>
2016-05-01 11:57:03 +0100

    Rename category.books handler in the controller

--- server_app/controllers/category.js
+++ server_app/controllers/category.js
@@ -1,8 +1,9 @@
-var categoryJson = function(req, res){
+var booksPerCategory = function(req, res){
   var books = [{category: "javascript", books: ['Angular in action', 'Getting MEAN']},
                {category: "scala", books: ['Scala in action', 'Getting Scala']}];
+ 
   res.status(200);
   res.json(books);
 }
 
-module.exports.books = categoryJson;
+module.exports.books = booksPerCategory;
