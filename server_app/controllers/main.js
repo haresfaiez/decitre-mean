14f775dbfaf6652632d5124df72d98ce13d152a3
Hares Faiez <faiez.hrs@gmail.com>
2016-05-01 08:44:03 +0100

    Add controller to fetch books of all the categories

--- server_app/controllers/main.js
+++ server_app/controllers/main.js
@@ -1,5 +1,11 @@
 var renderLayout = function(req, res){
                      res.render('layout', { title: 'Decitre.fr' });
                    }
+var categoryJson = function(req, res){
+  var books=[];
+res.status(400);
+  res.json(books);
+}
 
 module.exports.angular = renderLayout;
+module.exports.category = categoryJson;
--- server_app/routes/index.js
+++ server_app/routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var main = require('../controllers/main');
 
 router.get('/', main.angular);
+router.get('/category/books', main.category);
 
 module.exports = router;
