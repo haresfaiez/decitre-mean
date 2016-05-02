4b7b48377afaf4f458ad8892c5cf2552f2fb32c5
Hares Faiez <faiez.hrs@gmail.com>
2016-05-02 16:53:29 +0100

    Rename backend test case

--- test/server_app/spec/helloWorldSpec.js
+++ test/server_app/spec/helloWorldSpec.js
@@ -1,14 +1,12 @@
 var request = require("request");
 var fixture = require("../fixture/db_fixture");
 
-describe("Hello World Server", function() {
-  describe("GET /", function() {
-    it("returns status code 200", function(done) {
+describe("Category api", function() {
+    it("should fetch categories from database", function(done) {
       request.get("http://localhost:3000/category/books", function(error, response, body) {
         expect(response.statusCode).toEqual(200);
         expect(body).toEqual(fixture.category);
         done();
       });
     });
-  });
 });
