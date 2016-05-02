26e9c2b1ea1cbd2c28c1d361e2deada7b99fdd95
Hares Faiez <faiez.hrs@gmail.com>
2016-05-02 17:06:57 +0100

    Test existing book fetch api

---
+++ test/server_app/spec/controllers/bookSpec.js
@@ -0,0 +1,12 @@
+var request = require("request");
+var fixture = require("../fixture/db_fixture");
+
+describe("Book api", function() {
+    it("should fetch an existing from database", function(done) {
+      request.get("http://localhost:3000/book/5726173e78d1d8abc5e65f73", function(error, response, body) {
+        expect(response.statusCode).toEqual(200);
+        expect(body).toEqual(fixture.book.css_in_action);
+        done();
+      });
+    });
+});
--- test/server_app/spec/fixture/db_fixture.js
+++ test/server_app/spec/fixture/db_fixture.js
@@ -1,1 +1,4 @@
+module.exports.book               = {};
+module.exports.book.css_in_action = '{"_id":"5726173e78d1d8abc5e65f73","title":"css in action","category":{"_id":"572615eb78d1d8abc5e65f72","title":"css"}}';
+
 module.exports.category = '[{"category":"Scala in action","books":[]},{"category":"Scala","books":[]},{"category":"css","books":[{"_id":"5726173e78d1d8abc5e65f73","title":"css in action","category":"572615eb78d1d8abc5e65f72"},{"_id":"5726235b78d1d8abc5e65f74","title":"cssio","category":"572615eb78d1d8abc5e65f72"}]}]';
