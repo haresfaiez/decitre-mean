af5708a30fc6db036d8416edc8531d217901766b
Hares Faiez <faiez.hrs@gmail.com>
2016-05-02 16:49:23 +0100

    Test category fetch

--- test/server_app/spec/helloWorldSpec.js
+++ test/server_app/spec/helloWorldSpec.js
@@ -1,12 +1,12 @@
 var request = require("request");
-
-var base_url = "http://localhost:3000/"
+var fixture = require("../fixture/db_fixture");
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
+      request.get("http://localhost:3000/category/books", function(error, response, body) {
+        expect(response.statusCode).toEqual(200);
+        expect(body).toEqual(fixture.category);
         done();
       });
     });
