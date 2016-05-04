d8dbc6d65793ad69204cb0d450b85c105e05ae6a
Hares Faiez <faiez.hrs@gmail.com>
2016-05-04 09:14:34 +0100

    Fix test name

--- test/server_app/spec/controllers/bookSpec.js
+++ test/server_app/spec/controllers/bookSpec.js
@@ -2,7 +2,7 @@ var request = require("request");
 var fixture = require("../fixture/db_fixture");
 
 describe("Book api", function() {
-    it("should fetch an existing from database", function(done) {
+    it("should fetch an existing book from database", function(done) {
       request.get("http://localhost:3000/book/5726173e78d1d8abc5e65f73", function(error, response, body) {
         expect(response.statusCode).toEqual(200);
         expect(body).toEqual(fixture.book.css_in_action);
