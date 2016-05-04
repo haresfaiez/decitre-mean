61bc7b2198be7b5827a708cc6104d99747b674a9
Hares Faiez <faiez.hrs@gmail.com>
2016-05-04 13:45:34 +0100

    Organize book details Angular test

--- test/client_app/book/book.module_test.js
+++ test/client_app/book/book.module_test.js
@@ -1,5 +1,6 @@
 describe('Book Details', function(){
-  const angularBook = {_id: '0000', title: 'Angular 4'};
+  const angularBook    = {_id: '0000', title: 'Angular 4'};
+  const angularBookURL = '/book/' + angularBook._id;
 
   var $controller;
   var $backend;
@@ -7,18 +8,16 @@ describe('Book Details', function(){
   beforeEach(module('Decitre'));
 
   beforeEach(inject(function(_$controller_,
-                             _$httpBackend_,
-                             $injector){
+                             _$httpBackend_){
     $controller  = _$controller_;
     $backend     = _$httpBackend_;
   }));
 
   describe('fetch by by id', function(){
     it('should be binded to the view', function(){
-      var bookURL = '/book/' + angularBook._id;
-      $backend.whenGET(bookURL)
+      $backend.whenGET(angularBookURL)
               .respond(angularBook);
-      $backend.expectGET(bookURL);
+      $backend.expectGET(angularBookURL);
       var controller  = $controller('BindBook',
                                     {$routeParams: {bookid: angularBook._id}});
 
