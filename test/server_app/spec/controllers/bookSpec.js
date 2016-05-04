c0d74e3671cf9d21cda48f32874bcc4a43217485
Hares Faiez <faiez.hrs@gmail.com>
2016-05-04 14:05:06 +0100

    Format backend test

--- test/server_app/spec/controllers/bookSpec.js
+++ test/server_app/spec/controllers/bookSpec.js
@@ -1,11 +1,12 @@
 var request = require("request");
 var fixture = require("../fixture/db_fixture");
 
-describe("Book api", function() {
+describe("Book API", function() {
     it("should fetch an existing book from database", function(done) {
-      request.get("http://localhost:3000/book/5726173e78d1d8abc5e65f73", function(error, response, body) {
+      var css_in_actionURL= "http://localhost:3000/book/" + fixture.book.css_in_action._id;
+      request.get(css_in_actionURL, function(error, response, body) {
         expect(response.statusCode).toEqual(200);
-        expect(body).toEqual(fixture.book.css_in_action);
+        expect(body).toEqual(fixture.book.css_in_action.row);
         done();
       });
     });
--- test/server_app/spec/controllers/categorySpec.js
+++ test/server_app/spec/controllers/categorySpec.js
@@ -1,7 +1,7 @@
 var request = require("request");
 var fixture = require("../fixture/db_fixture");
 
-describe("Category api", function() {
+describe("Category API", function() {
     it("should fetch categories from database", function(done) {
       request.get("http://localhost:3000/category/books", function(error, response, body) {
         expect(response.statusCode).toEqual(200);
