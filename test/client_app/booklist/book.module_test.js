37e1dce1e96fdcb8fc1c6900bdce84c30f3c0349
Hares Faiez <faiez.hrs@gmail.com>
2016-05-04 08:53:51 +0100

    Test each of the service and the controller alone

--- test/client_app/booklist/book.module_test.js
+++ test/client_app/booklist/book.module_test.js
@@ -3,13 +3,17 @@ describe('Books', function(){
 
   var $controller;
   var $backend;
+  var $fetchBooks;
+  var $rootScope;
 
   beforeEach(module('Decitre'));
 
   beforeEach(inject(function(_$controller_,
-                             _$httpBackend_){
+                             _$httpBackend_,
+                             $injector){
     $controller  = _$controller_;
     $backend     = _$httpBackend_;
+    $fetchBooks  = $injector.get('FetchBooks');
   }));
 
   describe('from all categories', function(){
@@ -17,9 +21,17 @@ describe('Books', function(){
       $backend.whenGET('/category/books')
               .respond(someCategories);
       $backend.expectGET('/category/books');
-      var controller = $controller('BindCategoryBooks');
+      var handle = jasmine.createSpy();
 
+      $fetchBooks(handle)
       $backend.flush();
+
+      expect(handle).toHaveBeenCalledWith(someCategories);
+    });
+
+    it('should be binded to the view', function(){
+      var fakeService = function(handle){handle(someCategories);}
+      var controller  = $controller('BindCategoryBooks', {FetchBooks: fakeService});
 
       expect(controller.elements).toEqual(someCategories);
     });
