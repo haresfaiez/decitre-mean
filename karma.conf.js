f8ee1324599b659658458c636a8c5f1b5522f95c
Hares Faiez <faiez.hrs@gmail.com>
2016-05-07 10:37:08 +0100

    Watch just client app tests from Karma

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'client_app/**/*.js',
-      'test/**/*_test.js'
+      'test/client_app/**/*_test.js'
     ],
 
 
